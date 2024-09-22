d162cc989ade36906bd0bf1858c300f889bf8fb6
dimaxaz <145430196+dimaxaz@users.noreply.github.com>
2024-09-22 15:36:37 +0300

    task

---
+++ main.py
@@ -0,0 +1,18 @@
+#1st program
+print("1st program: ",(9**0.5)*5)
+
+#2nd program
+print("2st program: ",(9.99>9.98) and (1000 != 1000.1))
+
+#3st program
+a=2 * 2 + 2
+b= 2 * (2 + 2)
+
+print("3st program: ""1)",a,"\t2)",b,"\t3)", a==b)
+
+#4th program
+a='123.456'
+
+print("3st program: a = '123.456'",
+      "\nVariant 1) a[-3] =", a[-3],
+      "\nVariant 2) (int(float(a) * 10) % 10)= ", (int(float(a) * 10) % 10))
